71cf3c3f73f9b1dde9fc076e627881e894fd630e
okwuduche <chinweikeokwuduche@gmail.com>
2022-10-13 15:57:37 -0700

    Task 5

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - This is the entry point of every C program
+ * Return: (0)
+ */
+
+int main(void)
+{
+	puts("\"programming is like building a multilingual puzzle,\n");
+	return (0);
+}
