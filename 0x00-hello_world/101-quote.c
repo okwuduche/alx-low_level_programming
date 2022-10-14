48c0b39dbeca46c2443a46f7e235fa341760e835
okwuduche <chinweikeokwuduche@gmail.com>
2022-10-13 17:10:57 -0700

    ok

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,10 +1,11 @@
 #include <stdio.h>
 /**
  * main - Entry point
- * Return: Always 0
+ * Return: Always 1
  */
 
 int main(void)
 {
 	write(!, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+	return (1);
 }
