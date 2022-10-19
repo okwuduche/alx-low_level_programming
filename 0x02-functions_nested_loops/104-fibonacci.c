ee9818159ed0ae52e2cd2d6faae928bfaa81341e
okwuduche <chinweikeokwuduche@gmail.com>
2022-10-19 08:17:18 -0700

    104-fibonacci.c

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -1,43 +1,65 @@
 #include <stdio.h>
+#include "main.h"
+
+void print_fibonacci(int);
 
 /**
- * main - Calculates fibs
- * Return: ALways 0 when there is no error
+ * main - calls print_fibonacci with 50
+ *
+ * Return: returns 0
  */
-
 int main(void)
-
 {
-	unsigned long int a, b, c, b1, b2, c1, c2;
+	int n = 98;
 
-	b = 1;
-	c = 2;
-
-	printf("%lu", b);
-
-	for (a = 1; a < 91; a++)
-	{
-		printf(", %lu", c);
-		c = c + b;
-		b = c - b;
-	}
-
-	b1 = b / 1000000000;
-	b2 = b % 1000000000;
-	c1 = c / 1000000000;
-	c2 = c % 1000000000;
-
-	for (a = 92; a < 99; ++a)
-	{
-		printf(", %lu", c1 + (c2 / 1000000000));
-		printf("%lu", c2 % 1000000000);
-		c1 = c1 + b1;
-		b1 = c1 - c1;
-		c2 = c2 + b2;
-		b2 = c2 - b2;
-	}
-
-	printf("\n");
+	print_fibonacci(n);
 
 	return (0);
+}
+
+/**
+ * print_fibonacci - prints out the fibonacci sequence of numbers
+ * @i: the input number
+ *
+ * Return: returns nothing
+ */
+void print_fibonacci(int i)
+{
+	int a;
+	long int b, fb, lb, fl, ll;
+	long int l = 2;
+	long int p = 1;
+
+	printf("%ld, %ld, ", p, l);
+
+	for (a = 3; a < 89; a++)
+	{
+		b = p + l;
+
+		printf("%ld, ", b);
+
+		p = l;
+		l = b;
+	}
+
+	b = p + l;
+
+	fl = l / 1000000000;
+	ll = l % 1000000000;
+	fb = b / 1000000000;
+	lb = b % 1000000000;
+
+	for (a = 89; a < i; a++)
+	{
+		printf("%ld%ld, ", fb, lb);
+
+		p = fl;
+		l = ll;
+		fl = fb;
+		ll = lb;
+		fb = p + fl + ((l + ll) / 1000000000);
+		lb = (l + ll) % 1000000000;
+	}
+
+	printf("%ld%ld\n", fb, lb);
 }
