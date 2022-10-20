c4984102b6a92ce388215523a56941c29320a6b5
okwuduche <chinweikeokwuduche@gmail.com>
2022-10-20 06:33:36 -0700

    0-isupper.c

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -8,9 +8,9 @@
 
 int _isupper(int c)
 {
-	if (c >= &&c <= 90)
-	{
-		return (1);
-	}
-	return (0);
+        if (c >= &&c <= 90)
+        {
+                return (1);
+        }
+        return (0);
 }
