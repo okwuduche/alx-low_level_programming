7bac3abef7eaa1fd8787629aa295fa0a91f9ca74
okwuduche <chinweikeokwuduche@gmail.com>
2022-10-24 08:40:04 -0700

    0-reset_to_98.c

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -2,8 +2,7 @@
 #include "main.h"
 
 /**
-* main - code from intranet
-* @reset_to_98 - reset_to_98 i.e manipulates pointer's value
+* reset_to_98 - reset_to_98 i.e manipulates pointer's value
 * @n: Acts as pointer resetter
 * Return: void
 */
