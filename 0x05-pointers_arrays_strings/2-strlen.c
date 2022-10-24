d5e75b9dfb48b791fda79f0bfd74bbfe646e8186
okwuduche <chinweikeokwuduche@gmail.com>
2022-10-24 09:17:56 -0700

    2-strlen.c

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdio.h>
+#include "main.h"
+
+/**
+* _strlen - Used to return lenght of a sring
+* @s: Represents string
+* Return: Lenght of string
+*/
+
+int _strlen(char *s)
+
+{
+	size_t lenght = 0;
+
+	while (*++)
+	{
+		lenght++;
+	}
+	return (lenght);
+}
