f5b3b8a8a17c23da385750b7e350560681027ab8
okwuduche <chinweikeokwuduche@gmail.com>
2022-10-24 08:34:58 -0700

    0-reset_to_98.c

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+* main - code from intranet
+* @reset_to_98 - reset_to_98 i.e manipulates pointer's value
+* @n: Acts as pointer resetter
+* Return: void
+*/
+
+void reset_to_98(int *n)
+
+{
+	return (*n = 98);
+}
