eef4008329546c64131ddcecd73fe7e28ea116c5
okwuduche <chinweikeokwuduche@gmail.com>
2022-10-26 08:06:40 -0700

    0-strcat.c

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -9,19 +9,30 @@
 */
 
 char *_strcat(char *dest, char *src)
-
 {
-	int position = 0, dest_len = 0;
+	int index = 0, dest_len = 0;
 
-	while (dest[position = position + 1])
-	{
+	while (dest[index++])
 		dest_len++;
-	}
 
-	for (position = 0; src[position]; position++)
-	{
-		dest[dest_len++] = src[position];
-	}
+	for (index = 0; src[index]; index++)
+		dest[dest_len++] = src[index];
 
 	return (dest);
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
