b99cd72551bacd28b246abbb5b4e566e3ca4823d
okwuduche <chinweikeokwuduche@gmail.com>
2022-10-26 09:44:55 -0700

    7-leet.c

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -14,7 +14,7 @@
 
 	while (str[indx1])
 	{
-		for (indx2 = 0; indx2 <= (sizeof(leet) / sizeof(leet[0])); indx2++)
+		for (indx2 = 0; indx2 <= 7; indx2++)
 		{
 			if (str[indx1] == leet[indx2] ||
 			    str[indx1] - 32 == leet[indx2])
