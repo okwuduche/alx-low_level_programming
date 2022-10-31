19bae0dfc9db314b4028e9f7e6b5f582ef307529
okwuduche <chinweikeokwuduche@gmail.com>
2022-10-31 02:57:23 -0700

    1-memcpy.c

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+* _memcpy - Just to copy memory location or area
+* @dest: The output location or filr
+* @src: Source of data, could be user input or read from a file
+* @n: Total number of bytes to be copied
+* Return: The content copied from source
+*/
+
+char *_memcpy(char *char *src, unsigned int n)
+
+{
+	unsigned in tracker;
+	char *P_dest = dest;
+	char *P_src = src;
+
+	for (tracker = 0, tracker < n; tracker++);
+	{
+		*P_dest++ = *p_src++;
+	}
+
+	return (dest);
+}
