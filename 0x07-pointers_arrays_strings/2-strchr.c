a06a2eda06e77898c2f5eaf248671887f9f505ba
okwuduche <chinweikeokwuduche@gmail.com>
2022-10-31 03:37:17 -0700

    2-strchr.c

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+* _strchr - Locating character in a given string
+* @s: String from user
+* @c: The character
+* Return: Returns NULL if no character is found
+*/
+
+char *_strchr(char *s, char c)
+{
+	for (; ; s++)
+	{
+		if (*s == c)
+			return (s);
+		else if (*s == 0)
+			return (NULL);
+	}
+	return (NULL);
+}
