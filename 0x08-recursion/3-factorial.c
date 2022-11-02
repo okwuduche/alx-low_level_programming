35a9b0d673b5e41e59388140db53735878cac975
okwuduche <chinweikeokwuduche@gmail.com>
2022-11-02 09:22:29 -0700

    5-sqrt_recursion.c

--- 0x08-recursion/3-factorial.c
+++ 0x08-recursion/3-factorial.c
@@ -1,9 +1,9 @@
 #include "main.h"
 
 /**
- * factorial - Function that gets factorial of n instead of using for loop iteration
+ * factorial - Function that gets factorial of n
+ * instead of using for loop iteration
  * @n: Represents integer
- *
  * Return: 1 when successful -1 when otherwise
  */
 int factorial(int n)
