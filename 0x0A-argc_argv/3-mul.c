3442d1e080b5306dc75b5571a7e7f51a9a3e0fe7
okwuduche <chinweikeokwuduche@gmail.com>
2022-11-04 13:23:19 -0700

    3-mul.c

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,27 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - Function that multiplies inputed numbers
+ * @argc: arguments counted for main
+ * @argv: element
+ * Return: void (empty)
+ */
+
+int main(int argc, char *argv[])
+{
+	int multiplier = 0;
+
+	if (argc == 3)
+	{
+		multiplier = atoi(argv[1]) * atoi(argv[2]);
+		printf("%d\n", mul);
+		return (0);
+	}
+	else
+	{
+		printf("Error\n");
+		return (1);
+	}
+}
