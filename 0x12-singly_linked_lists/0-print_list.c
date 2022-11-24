9710b07c2a771deb7077d70934ed1cfb525f1d0b
okwuduche <chinweikeokwuduche@gmail.com>
2022-11-24 06:29:23 -0800

    0-print_list.c

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * print_list - Function that prints elements of a list.
+ * @h: singly linked list.
+ * Return: number of elements in the list.
+ */
+
+size_t print_list(const list_t *h)
+{
+	unsigned int num = 0;
+
+	while (h)
+	{
+		if (h->str == NULL)
+			printf("[%d] %s\n", 0, "(nil)");
+		else
+			printf("[%d] %s\n", h->len, h->str);
+		h = h->next;
+		num++;
+	}
+	return (num);
+}
---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * list_len - returns then number of elements in a list.
+ * @h: singly linked list.
+ * Return: number of elements in the list.
+ */
+
+size_t list_len(const list_t *h)
+{
+	unsigned int integer = 0;
+
+	while (h)
+	{
+		h = h->next;
+		integer++;
+	}
+	return (integer);
+}
